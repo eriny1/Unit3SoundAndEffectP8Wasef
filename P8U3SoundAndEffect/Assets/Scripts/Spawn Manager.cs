df791e208f26ad0202bd590ca42e6c58bede332b
eriny1 <erenynader734@gmail.com>
2025-04-28 15:30:15 -0700

    U3
    Lesson 3.3 - Don't Just Stand There

--- P8U3SoundAndEffect/Assets/Scripts/Spawn Manager.cs
+++ P8U3SoundAndEffect/Assets/Scripts/Spawn Manager.cs
@@ -8,20 +8,25 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnpos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repearRate = 2;
+    private PlayerController PlayerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repearRate);
+        PlayerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     void SpawnObstacle()
     {
-        Instantiate(obstaclPrefab, spawnpos, obstaclPrefab.transform.rotation);
-    }
+        if (PlayerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclPrefab, spawnpos, obstaclPrefab.transform.rotation);
+        }
 
     }
+}
